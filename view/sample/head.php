43c178d9d5a83a01cd65154993e2bb957908b897
bastienbouge <bastien-bouge@hotmail.fr>
2021-01-05 17:54:54 +0100

    add color when done

--- view/sample/head.php
+++ view/sample/head.php
@@ -9,14 +9,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="shortcut icon" href="https://cdn.icon-icons.com/icons2/2331/PNG/512/todolist_planing_list_planlist_todo_icon_142265.png" />
-
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+   
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"/>
 
     <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" data-auto-replace-svg="nest"></script>
-
+    
     <link rel="stylesheet" href="./view/css/style.css"/>
 </head>
--- view/toDoList.php
+++ view/toDoList.php
@@ -22,9 +22,7 @@
             <?php
             $num = 0;
             foreach ($list as $lis){
-             
             
-          
                 echo '<div class="card">
                             <div class="card-header" id="headingOne">
                                 <h5 class="mb-0">
@@ -54,8 +52,10 @@
                       ';
                 $num = $num+1;
                 foreach ($lis->getTaskArray() as $t){
+
                     $settings = '
                        
+
                         <form method="POST" style="display: inline-block;">
                         <button type="submit"  name="action" value="delTask" class="btn btn-danger" style="width: 2.5em"><i class="fas fa-times"></i></button>
                         <input type="hidden" name="id" value="'.$t->getId().'">
@@ -66,15 +66,17 @@
                         <input type="hidden" name="id" value="'.$t->getId().'">
                         </form>
                     ';
-                    
-                    echo '<tr style="background-color: '.$t->getColor().';">
-                        <div id="jquery">
+                 
+
+                    echo '<tr class="'.$t->isDone().'" style="background-color: '.$t->getColor().';">
+                        <div>
                                 <td>'.$t->getTitre().'</td>
                                 <td>'.$t->getDescription().'</td>
                         </div>        
                                 <td class="hiddenButton">'.$settings.'</td>
                             </tr>
                     ';
+                 
                 }
                       echo '</tbody></table></div></div></div>';
             }
@@ -82,9 +84,9 @@
     </div>
 </body>
 
-<script>
 
-$(document).on('click', 'td', function() {
-  $(this).toggleClass("br");
-});
-</script>
+<script type="text/javascript">
+                     $(document).ready(function () {
+                        $(".1").css("background-color", "#A6A4A8");
+                    });
+ </script> 
